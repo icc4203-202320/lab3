cfd4b7cada0069a43740d002676a190773806040
DanielMilstein <dnmilstein@miuandes.cl>
2023-01-26 21:47:15 -0300

    Lab 3 Base

--- src/components/TripPage/TripPage.js
+++ src/components/TripPage/TripPage.js
@@ -5,20 +5,9 @@ import gql from 'graphql-tag';
 
 
 function TripPage() {
-    // const { loading, error, data } = useQuery(GET_DATA);
-    //
-    // if (loading) return <p>Loading...</p>;
-    // if (error) return <p>Error: {error.message}</p>;
 
     return (0
-        // <div>
-        //     <h1>Data Index</h1>
-        //     {data.map(item => (
-        //         <div key={item.id}>
-        //             <a href={`/data/${item.id}`}>{item.name}</a>
-        //         </div>
-        //     ))}
-        // </div>
+
     );
 }
 
